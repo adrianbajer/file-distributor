f1df9fb8297326686222f0dbda7fee35b44d762e
Adrian Bajer <geograf33@gmail.com>
2019-12-21 19:18:17 +0100

    ExcelParser has logic to get all necessary data from Excel

--- src/main/java/excel/ExcelParser.java
+++ src/main/java/excel/ExcelParser.java
@@ -1,16 +1,12 @@
 package excel;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class ExcelParser {
 
@@ -21,9 +17,10 @@ public class ExcelParser {
         String userInputRaksCode = "Akwitania._map_LAM9_FR";
 
         List <String> zasiegiKorektyList = new ArrayList<>();
-        List <String> plikiZrodloweList = new ArrayList<>();
+        Set <String> plikiZrodloweSet = new HashSet<>();
         Map<String, String> plikiZrodloweRegionMap = new HashMap<>();
         Map<String, String> plikiZrodloweRodzajMap = new HashMap<>();
+        Map<String, String> plikiZrodloweMiejsceMap = new HashMap<>();
 
 
         try {
@@ -45,53 +42,58 @@ public class ExcelParser {
             Sheet zasiegiKorekty = workbook.getSheetAt(2);
 
 
+//            @@@@@@@@@@@@@ this loop gets names of "zasiegi korekty" from "wydania" sheet,
+//            which has RaksCode equal to RaksCode given by user @@@@@@@@@@@@@
+
             for(Row row : wydania) {
-
-                // condition skips 3 first rows, which contains unnecessary combined cells and headings
+                // first condition skips 3 first rows, which contains headings
                 if (row.getRowNum() > 2) {
-                    for (Cell cell : row) {
-//                        if (cell.getAddress().toString().startsWith("A") || cell.getAddress().toString().startsWith("L")) {
-                        if (cell.getAddress().toString().startsWith("A") && cell.getStringCellValue().equals(userInputRaksCode)) {
-                            zasiegiKorektyList.add(row.getCell(11).toString());
-
-//                            switch (cell.getCellType()) {
-//                                case NUMERIC:
-//                                    System.out.print(cell.getNumericCellValue() + " ");
-//                                    break;
-//                                case STRING:
-//                                    System.out.print(cell.getStringCellValue() + " ");
-//                                    break;
-//                            }
-                        }
+                    if (row.getCell(0).toString().equals(userInputRaksCode)) {
+                        zasiegiKorektyList.add(row.getCell(11).toString());
                     }
                 }
             }
-            System.out.println(zasiegiKorektyList);
 
-//         @@@@@@@@@@@@@ loop gets names of source files from "zasiegi_korekty" sheet,
-//            basing on "zasiegiKorektyList" values, which are primary key @@@@@@@@@@@@@
+//         @@@@@@@@@@@@@ this loop gets names of "pliki zrodlowe" from "zasiegi_korekty" sheet,
+//            basing on "zasiegiKorektyList" values, which are primary key in that sheet.
+//            Set is used to get unique names of "pliki zrodlowe".
+//            There are also created two Maps, which contains data for preparing path to files.
+//            Maps will be used later in the FileDistributor application @@@@@@@@@@@@@
             for(Row row : zasiegiKorekty) {
                 if (row.getRowNum() > 1) {
-                    for (Cell cell : row) {
-//                        if (cell.getAddress().toString().startsWith("A") || cell.getAddress().toString().startsWith("L")) {
-                        if (cell.getAddress().toString().startsWith("A") && cell.getStringCellValue().equals(userInputRaksCode)) {
-                            zasiegiKorektyList.add(row.getCell(11).toString());
+                    String cellValue = row.getCell(0).getStringCellValue();
+                    for(String zasiegKorekty : zasiegiKorektyList) {
+                        if(cellValue.equals(zasiegKorekty)) {
+                            plikiZrodloweSet.add(row.getCell(1).toString());
+                            plikiZrodloweRegionMap.put(row.getCell(1).toString(), row.getCell(15).toString());
+                            plikiZrodloweRodzajMap.put(row.getCell(1).toString(), row.getCell(16).toString());
+                            break;
+                        }
+                    }
+                }
+            }
 
-//                            switch (cell.getCellType()) {
-//                                case NUMERIC:
-//                                    System.out.print(cell.getNumericCellValue() + " ");
-//                                    break;
-//                                case STRING:
-//                                    System.out.print(cell.getStringCellValue() + " ");
-//                                    break;
-//                            }
+//            @@@@@@@@@@@@ this loop gets place name (not path!) where "plik zrodlowy" is.
+//            Place name is put to the Map @@@@@@@@@@@@@
+
+            for(Row row : plikiZrodlowe) {
+                if (row.getRowNum() > 1) {
+                    String cellValue = row.getCell(0).getStringCellValue();
+                    for(String plikZrodlowy : plikiZrodloweSet) {
+                        if(cellValue.equals(plikZrodlowy)) {
+                            plikiZrodloweMiejsceMap.put(row.getCell(0).toString(), row.getCell(3).toString());
+                            break;
                         }
                     }
                 }
             }
 
 
-
+            System.out.println(zasiegiKorektyList);
+            System.out.println(plikiZrodloweSet);
+            System.out.println(plikiZrodloweRegionMap);
+            System.out.println(plikiZrodloweRodzajMap);
+            System.out.println(plikiZrodloweMiejsceMap);
 
 
             file.close();
@@ -99,31 +101,6 @@ public class ExcelParser {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-        // @@@@@@@@@@@ another way of iterating @@@@@@@@@@@@@@
-
-        //            Iterator<Row> rowIterator = sheet.iterator();
-//            while (rowIterator.hasNext()) {
-//                Row row = rowIterator.next();
-//
-//                Iterator<Cell> cellIterator = row.cellIterator();
-//
-//                while (cellIterator.hasNext()) {
-//                    Cell cell = cellIterator.next();
-//
-//                    switch (cell.getCellType()) {
-//                        case NUMERIC:
-//                            System.out.print(cell.getNumericCellValue() + " ");
-//                            break;
-//                        case STRING:
-//                            System.out.print(cell.getStringCellValue() + " ");
-//                            break;
-//                    }
-//                }
-//                System.out.println("");
-
-
 
     }
 }
