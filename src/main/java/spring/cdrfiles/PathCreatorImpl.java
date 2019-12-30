fb187d58b4a622b3f1cf844e429b63aff1a4dbef
Adrian Bajer <geograf33@gmail.com>
2019-12-30 15:18:00 +0100

    Add implementation for PathCreatorImpl class and add example file repository

--- src/main/java/spring/cdrfiles/PathCreatorImpl.java
+++ src/main/java/spring/cdrfiles/PathCreatorImpl.java
@@ -2,9 +2,19 @@ package spring.cdrfiles;
 
 public class PathCreatorImpl implements PathCreator{
 
+
     @Override
     public String createPath(CdrFile cdrFile) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("src\\main\\resources\\example file repository\\");
+        sb.append(cdrFile.getType() + "\\");
+        sb.append(cdrFile.getRegion() + "\\");
+        sb.append(cdrFile.getName() + ".txt");
+
+        return sb.toString();
     }
+
 
 }
